9ad5c3c419aa19f21abddcc2fdff03af0df0072d
Jiten <jitenshreshtha07@gmail.com>
2022-05-01 20:15:07 +0545

    home component created.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <p>This is my last tutorial.</p>
+      <Home />
     </div>
   );
 }
